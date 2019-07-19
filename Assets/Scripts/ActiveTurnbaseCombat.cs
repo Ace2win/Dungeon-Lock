e569ef32f3da56606ae7295d0e6d3c7efcda5879
Ace <Ace2win@hotmail.de>
2019-07-19 05:10:26 +0200

    UI + Spawn
    Created the Spawn System with changeable Spawnpoints and started on the HUD which indicates Player HP and ATB

---
+++ Assets/Scripts/ActiveTurnbaseCombat.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ActiveTurnbaseCombat : MonoBehaviour
+{
+    #region Variables
+    float Globalroundtime = 0;
+
+    float Player_ActiveCombatbar  = 0;
+    float[] Monster_ActiveCombatbars;
+
+    float currentATB = 0;
+    float maxATB = 10;
+
+    bool ready;
+    #endregion
+
+    #region
+    Slider ATB_Bar;
+    Slider LIFE_Bar;
+    Slider BOSS_Bar;
+    #endregion
+
+    GameObject I_Player;
+
+    void Start()
+    {
+        Setup();
+
+        Globalroundtime += Time.deltaTime;
+    }
+
+    void Setup()
+    {
+        I_Player = GameObject.FindGameObjectWithTag("Player");
+        ATB_Bar =  GameObject.FindGameObjectWithTag("ATB_Bar").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void ATB()
+    {
+        if(currentATB >= maxATB)
+        {
+            ready = true;
+            // Frag Command aus der Liste ab
+            // Reset Current ATB to 0
+            //return;
+        }
+        else
+        {
+            ready = false;
+            currentATB += Time.deltaTime;
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameCore.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCore : MonoBehaviour
+{
+    public static int Level_ID;
+
+    [SerializeField]
+    GameObject I_Player;
+
+    [SerializeField]
+    GameObject I_Boss;
+   
+    GameObject Boss_Spawn;
+    GameObject Player_Spawn;
+
+    [SerializeField]
+    Vector3 Monster_Spawnpoint = new Vector3 (0,  2, 0);
+    [SerializeField]
+    Vector3 Player_Spawnpoint  = new Vector3 (0, -1, 0);
+    
+
+    void Awake()
+    {
+        Spawning();
+    }
+
+    void Spawning()
+    {
+        Player_Spawn = GameObject.FindGameObjectWithTag("Player_Spawn");
+        Boss_Spawn   = GameObject.FindGameObjectWithTag("Boss_Spawn");
+
+        Player_Spawn.transform.position = Player_Spawnpoint;
+        Instantiate(I_Player,Player_Spawn.transform.position,Quaternion.identity);
+        Boss_Spawn.transform.position = Monster_Spawnpoint;
+        Instantiate(I_Boss, Boss_Spawn.transform.position,Quaternion.identity);
+    }
+
+    void Start()
+    {
+    //  Find Correct Monster corresponed to Level.
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
